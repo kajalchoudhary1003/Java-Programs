677725f65322d6a058bce2419355bc48022f327e
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2023-01-09 16:07:57 +0530

    Create doWhileLoop.java

---
+++ doWhileLoop.java
@@ -0,0 +1,11 @@
+public class dowhile0to10 {
+    public static void main(String args[]) {
+        int i=0;
+        do{
+            System.out.println(i);
+            i=i+1;
+        }while(i<11);
+        
+    }
+    
+}
