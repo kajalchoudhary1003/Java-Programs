25a0bc2fa7fadbf531e9b5422b477a9dcf8e9904
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2023-01-09 16:04:40 +0530

    Create whileLoop.java

---
+++ whileLoop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class while0to10 {
+    public static void main(String args[]) {
+try(Scanner sc= new Scanner(System.in)){
+    int i=0;
+    while(i<11){
+        System.out.println(i);
+        i++;
+    }
+}        
+    }
+    
+}
