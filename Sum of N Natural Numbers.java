2a18e9fb882f8082c0eee7c9372be7709d975cee
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2023-01-09 16:10:14 +0530

    Create Sum of N Natural Numbers.java

---
+++ Sum of N Natural Numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class naturalNoSum {
+    public static void main(String args[]){
+        try(Scanner sc= new Scanner(System.in)){
+            int n=sc.nextInt();
+            int sum=0;
+            for(int i=0; i<=n; i++){
+                sum=sum+i;
+            }
+            System.out.println(sum);
+        }
+    }
+    
+}
