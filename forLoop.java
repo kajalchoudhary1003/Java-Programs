c32a3ed40116daf9aea42c9611b72046c834a917
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2023-01-09 16:06:04 +0530

    Create forLoop.java

---
+++ forLoop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class for0to10 {
+    public static void main(String args[]) {
+        try(Scanner sc=new Scanner(System.in)){
+            for(int i=0; i<11; i++){
+                System.out.println(i);
+            }
+        }
+        
+    }
+    
+}
